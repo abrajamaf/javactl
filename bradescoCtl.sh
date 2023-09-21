cdf2a5cbc21de3af63a30c4007fc95e657963740
Abraham Alvarado Fuentes <5877076+abrajamaf@users.noreply.github.com>
2023-09-20 23:34:57 -0600

    Add menu in certCtl.sh

--- bradescoCtl.sh
+++ bradescoCtl.sh
@@ -4,46 +4,48 @@ set -eox pipefail
 # RJO='\e[1;31m'
 VDE='\e[1;32m'
 AMA='\e[1;33m'
-AZL='\e[1;34m'
+# AZL='\e[1;34m'
 NTRO='\e[0m'
 # BLNK='\e[5m'
 
 function menu() {
-  echo -e " 1 =$VDE Despliega$NTRO archivo jar en el servidor \"principal\""
+  echo -e " 1 =$VDE Despliega$NTRO archivo jar en el servidor \"principal.\""
+  echo -e " 2 =$VDE Opciones $NTRO de controles sobre los servicios$AMA Jar$NTRO."
+  echo -e " q =$VDE Salir. $NTRO"
+  echo -e "\n"
+}
+
+function deployment() {
+  # copia y despliega el archivo jar en el servidor "principal"
+  echo -e " Ésta opción redirige el tráfico a un$AMA solo Servidor$NTRO "
+  echo -e " ¿Está seguro de aplicar el despliegue?"
+  echo -e " Si esta seguro digite$AMA s$NTRO :"
+  read -r ABC
+  if [[ "${ABC}" == "s" ]]; then
+    find "$HOME" -name "*.jar"
+    echo " Escriba el nombre del archivo: "
+    read -r jarFile
+    # ssh -t $NGINX 'sudo /etc/nginx/conf.d/upstream/nodos/oneNode.sh'  ## Cambia Cluster a un nodo
+    scp -p -P 2290 "$HOME"/"$jarFile" "$HOST":/BID/bdco-servicios/deployment/deppot/
+    ssh -t "$HOST" sudo /BID/bdco-servicios/tools/deployment.sh
+    exit 0
+  fi
+  exit 1
+}
+
+function cert() {
   echo -e " 2 =$VDE Sincroniza$NTRO los archivos$AMA Jar$NTRO al servidor correspondiente"
   echo -e " 3 =$VDE Detiene$NTRO todos loa servicios Java desplegados en el Cluster"
   echo -e " 4 =$VDE Inicoa$NTRO todos loa servicios Java desplegados en el CLuster"
   echo -e " 5 =$VDE Reinicia$NTRO todos loa servicios Java desplegados en el CLuster"
   echo -e " 6 =$VDE Muestra el estado$NTRO de los servicios Java desplegados en el servidor"
   echo -e " 7 =$VDE Es una prueba.$NTRO"
-}
-
-function prod() {
-menu
-read -p " Elija una opción: " OPTION
-  grep -v '^ *#' <nodos-prod.txt | while IFS= read -r line; do
-    ssh "$line" 'bash -s' <prodServices.sh $OPTION
-  done
-}
-function cert() {
-  menu
-  read -p " Elija una opción: " OPTION
+  echo -e " Elija una opción: "
+  echo -e "\n"
+  read -r OPTION
   grep -v '^ *#' <nodos-cert.txt | while IFS= read -r line; do
-    ssh -t "$line" 'bash -s' <certServices.sh $OPTION
+    ssh -t "$line" 'bash -s' <certServices.sh  "$OPTION"
   done
-}
-
-function enviroment() {
-  if [[ $OPT == "prod" ]]; then
-    HOST='172.20.130.110'
-    prod
-
-  elif [[ $OPT == "cert" ]]; then
-    HOST='172.20.138.10'
-    cert
-  else
-    echo "El server no existe"
-  fi
 }
 
 # if [ "$1" = "--help" ]; then
@@ -51,17 +53,17 @@ function enviroment() {
 #   exit 0
 # fi
 while [[ $OPT != q ]]; do
-  echo Hola
+  menu
   read -r OPT
   case "$OPT" in
-  prod)
-    enviroment
-    echo -e " ----------------------------------------------------------------------------- "
+  1)
+    deployment
+    echo " ----------------------------------------------------------------------------- "
     read -p "Pulse cualquier tecla para continuar ..." any
     ;;
-  cert)
-    enviroment
-    echo -e " ----------------------------------------------------------------------------- "
+  2)
+    cert
+    echo " ----------------------------------------------------------------------------- "
     read -p "Pulse cualquier tecla para continuar ..." any
     ;;
   q)
@@ -69,14 +71,10 @@ while [[ $OPT != q ]]; do
     clear && exit 0
     ;;
   *)
-    echo "Usar: $0 {1|2|3|4|5|6}"
-    echo -e "1 =$VDE Despliega$NTRO archivo jar en el servidor \"principal\""
-    echo -e "2 =$VDE Sincroniza$NTRO los archivos$AMA Jar$NTRO al servidor correspondiente"
-    echo -e "3 =$VDE Detiene$NTRO todos loa servicios Java desplegados en el servidor"
-    echo -e "4 =$VDE Inicoa$NTRO todos loa servicios Java desplegados en el servidor"
-    echo -e "5 =$VDE Reinicia$NTRO todos loa servicios Java desplegados en el servidor"
-    echo -e "6 =$VDE Muestra el estado de los servicios Java desplegados en el servidor"
-    echo -e "6 =$VDE Muestra el estado de los servicios Java desplegados en el servidor"
+    echo "Usar: $0 {1|2}"
+    echo -e " 1 =$VDE Despliega$NTRO archivo jar en el servidor \"principal.\""
+    echo -e " 2 =$VDE Opciones $NTRO de controles sobre los servicios$AMA Jar$NTRO."
+    echo -e " q =$VDE Salir. $NTRO"
     exit 1
     ;;
   esac
--- certServices.sh
+++ certServices.sh
@@ -2,14 +2,14 @@
 set -eox pipefail
 
 # Colores
-RJO='\e[1;31m'
+# RJO='\e[1;31m'
 VDE='\e[1;32m'
 AMA='\e[1;33m'
 # AZL='\e[1;34m'
 NTRO='\e[0m'
 # BLNK='\e[5m'
 HOST='172.20.138.10'
-NGINX='172.20.138.7'
+
 function test() {
   echo -e "$AMA${HOSTNAME^^}$NTRO  $(hostname -I)"
   sudo su
@@ -18,26 +18,10 @@ function test() {
   for f in *.jar; do
     [[ -e "$f" ]] || break
     pwd
-    echo -e "scp -P 2290 -p root@"$HOST":/BID/bdco-servicios/jar/"$f" ."
+    echo -e "scp -P 2290 -p root@$HOST:/BID/bdco-servicios/jar/$f ."
     ls -la /BID/bdco-servicios/jar/"$f"
   done
   exit 0
-}
-
-function deployment() {
-  # copia y despliega el archivo jar en el servidor "principal"
-  echo -e " Ésta opción redirige el tráfico a un$AMA solo Servidor$NTRO "
-  echo -e " ¿Está seguro de aplicar el despliegue?"
-  echo -e " Si esta seguro digite$AMA s$NTRO :"
-  read -r ABC
-  if [[ "${ABC}" == "s" ]]; then
-    find "$HOME" -name "*.jar"
-    read -p "Escriba el nombre del archivo: " jarFile
-    ssh -t $NGINX 'sudo /etc/nginx/conf.d/upstream/nodos/oneNode.sh'
-    scp -p -P 2290 "$HOME/$jarFile" "$HOST":/BID/bdco-servicios/deployment/deppot/
-    ssh -t "$HOST" sudo /BID/bdco-servicios/tools/deployment.sh
-  fi
-  exit
 }
 
 function syncJar() {
@@ -71,6 +55,7 @@ function startAll() {
   sh start-all-services.sh
   exit 0
 }
+
 function restartAll() {
   # reinica todos los servicios Java desplegado en un servidor
   echo -e "$AMA${HOSTNAME^^}$NTRO  $(hostname -I)"
@@ -88,52 +73,33 @@ function status() {
   exit 0
 }
 
-function menu() {
-  echo -e " Script de control del ambiente$RJO Bradesco CERT$NTRO"
-  echo -e " 1 =$VDE Despliega$NTRO archivo jar en el servidor \"principal\""
+case "${1}" in
+2)
+  syncJar
+  ;;
+3)
+  stopAll
+  ;;
+4)
+  startAll
+  ;;
+5)
+  restartAll
+  ;;
+6)
+  status
+  ;;
+7)
+  test
+  ;;
+*)
+  echo "Usar: $0 {2|3|4|5|6|7}"
   echo -e " 2 =$VDE Sincroniza$NTRO los archivos$AMA Jar$NTRO al servidor correspondiente"
   echo -e " 3 =$VDE Detiene$NTRO todos loa servicios Java desplegados en el Cluster"
   echo -e " 4 =$VDE Inicoa$NTRO todos loa servicios Java desplegados en el CLuster"
   echo -e " 5 =$VDE Reinicia$NTRO todos loa servicios Java desplegados en el CLuster"
   echo -e " 6 =$VDE Muestra el estado$NTRO de los servicios Java desplegados en el servidor"
   echo -e " 7 =$VDE Es una prueba.$NTRO"
-}
-
-# while [[ $1 != q ]]; do
-  # menu
-  # read -r ACTION
-  case "${1}" in
-  1)
-    deployment
-    ;;
-  2)
-    syncJar
-    ;;
-  3)
-    stopAll
-    ;;
-  4)
-    startAll
-    ;;
-  5)
-    restartAll
-    ;;
-  6)
-    status
-    ;;
-  7)
-    test
-    ;;
-  *)
-    echo "Usar: $0 {1|2|3|4|5|6|7}"
-    echo -e " 1 =$VDE Despliega$NTRO archivo jar en el servidor \"principal\""
-    echo -e " 2 =$VDE Sincroniza$NTRO los archivos$AMA Jar$NTRO al servidor correspondiente"
-    echo -e " 3 =$VDE Detiene$NTRO todos loa servicios Java desplegados en el Cluster"
-    echo -e " 4 =$VDE Inicoa$NTRO todos loa servicios Java desplegados en el CLuster"
-    echo -e " 5 =$VDE Reinicia$NTRO todos loa servicios Java desplegados en el CLuster"
-    echo -e " 6 =$VDE Muestra el estado$NTRO de los servicios Java desplegados en el servidor"
-    echo -e " 7 =$VDE Es una prueba.$NTRO"
-    exit 1
-    ;;
-  esac
-# done
+  exit 1
+  ;;
+esac
